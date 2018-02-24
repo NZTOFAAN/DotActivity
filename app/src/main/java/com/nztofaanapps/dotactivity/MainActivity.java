7cc84384fcd35c3e372e7abf141765bc3e46493c
NZTOFAAN <36606472+NZTOFAAN@users.noreply.github.com>
2018-02-24 06:20:19 +0500

    Add files via upload

--- app/src/main/java/com/nztofaanapps/dotactivity/MainActivity.java
+++ app/src/main/java/com/nztofaanapps/dotactivity/MainActivity.java
@@ -1,1 +1,29 @@
+package com.nztofaanapps.dotactivity;
 
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+import android.net.*;
+import android.text.method.*;
+import android.view.View.*;
+import android.view.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		
+		String fileName="video";
+		String file="android.resource://" + getPackageName() + "/raw/"+fileName;
+		
+		final VideoView mVideoView=(VideoView) findViewById(R.id.mainVideoView);
+		mVideoView.setVideoURI(Uri.parse(file));
+		mVideoView.start();
+		
+		mVideoView.getKeepScreenOn();
+	mVideoView.setMediaController(new MediaController(this));
+    }
+}
